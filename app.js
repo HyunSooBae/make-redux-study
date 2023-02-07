d3d6d5dfee08b7eab3bec5b319db4c436bcd5938
hyunsooBae <hyunsoobae.dev@gmail.com>
2023-02-07 20:41:38 +0900

    좀 더 리덕스스럽게...2

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ import * as Actions from "./actions";
 const store = createStore(reducer);
 
 store.subscribe(function() {
-  console.log(store.getState());
+  console.log('subscribe => ', store.getState());
 });
 
 store.dispatch(Actions.increase());
+console.log('dispatch1 => ', store.getState());
 store.dispatch(Actions.increase());
+console.log('dispatch2 => ', store.getState());
 store.dispatch(Actions.increase());
+console.log('dispatch3 => ', store.getState());
 store.dispatch(Actions.decrease());
+console.log('dispatch4 => ', store.getState());
 store.dispatch(Actions.reset());
+console.log('dispatch5 => ', store.getState());
